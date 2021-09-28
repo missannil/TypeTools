54179a2bbe31731b376519a19e04ed18e9188335
missannil <missannil@163.com>
2021-09-28 09:55:04 +0800

    init

---
+++ Object/RequiredKeys.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @hidden
+ */
+export declare type _RequiredKeys<O extends object> = {
+    [K in keyof O]-?: {} extends Pick<O, K> ? never : K;
+}[keyof O];
+/**
+ * Get the keys of `O` that are required
+ * @param O
+ * @returns [[Key]]
+ * @example
+ * ```ts
+ * ```
+ */
+export declare type RequiredKeys<O extends object> = O extends unknown ? {
+    [K in keyof O]-?: {} extends Pick<O, K> ? never : K;
+}[keyof O] : never;
