f692220c7c6addfb15c29ffddbbe814cde81e7f3
missannil <missannil@163.com>
2021-10-18 19:50:11 +0800

    fix IfEquals Impossible

--- Any/Impossible.d.ts
+++ Any/Impossible.d.ts
@@ -1,8 +1,20 @@
+import { IfEquals } from "./IfEquals";
 
 /**
- * @param K 指定对象字段
+ * @param K 指定的key
  * @returns 返回指定字段为never的对象
+ * @template
+ * ```ts
+ * type test0 = Impossible<'name'|'age'>
+ * // type test0 = { name?: never;age?: never;} 
+ * type test1 = Impossible<never>
+ * // type test1 = unknown
+
+ * ```
  */
-export type Impossible<K extends Key> = {
-    [P in K]: never;
-};
+export type Impossible<K> = IfEquals<K, never, unknown, {
+    [P in K]?: never;
+}>;
+
+
+
